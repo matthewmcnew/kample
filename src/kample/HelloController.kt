4a12a32f4869b746f27bea5f6db44c42d37846f7
Matthew McNew <matthewmcnew@gmail.com>
2016-06-04 14:35:17 -0500

    Some autowiring

--- src/kample/HelloController.kt
+++ src/kample/HelloController.kt
@@ -1,15 +1,32 @@
 package kample
 
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class HelloController {
+class HelloController @Autowired constructor(val helloRepo: HelloRepository) {
 
     @RequestMapping("/")
     fun hello(): Hello {
-        return Hello("how are you?")
+        val name = helloRepo.get()
+
+        return Hello(name)
     }
 }
 
-data class Hello(val message: String)
+
+@Component
+class HelloRepository {
+
+    fun get(): String {
+        return "Jimmy"
+    }
+
+}
+
+class Hello(name: String) {
+    val hello = "hi " + name
+    val bye = "bye " + name
+}
